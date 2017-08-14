84bedbee842cf921f265707607fabe2b1b1d6740
KajanM <kajan.14@cse.mrt.ac.lk>
2017-08-14 16:39:51 +0530

    data retrival using services and dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {ProductListComponent} from "./products/product-list.component";
 import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";

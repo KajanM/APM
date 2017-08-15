278692cde1a8363638ad5f9f9e9efd8dd73e4ee4
kajan <kajan.mohanagandhirasa@proavos.com>
2017-08-15 18:00:06 +0530

    advanced routing with parameter and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,30 @@ import {FormsModule} from "@angular/forms";
 import {ConvertToSpacesPipe} from "./shared/convert-to-spaces.pipe";
 import {StarComponent} from "./shared/star.component";
 import {HttpClientModule} from "@angular/common/http";
+import { ProductDetailComponent } from './products/product-detail.component';
+import {WelcomeComponent} from "./home/welcome.component";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule, FormsModule, HttpClientModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: "products", component: ProductListComponent},
+      {path: "products/:id", component: ProductDetailComponent},
+      {path: "welcome", component: WelcomeComponent},
+      {path: "", redirectTo: "welcome", pathMatch: "full"},
+      {path: "**", redirectTo: "welcome", pathMatch: "full"}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
